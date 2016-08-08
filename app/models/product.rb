6f2a258a5aa33dea69b10202320f69b495cd2c88
Sherfin S <sherfin94@gmail.com>
2016-08-08 14:14:15 +0530

    Apply fragment caching to product list

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ApplicationRecord
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a gif, hpg or png'
   }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
