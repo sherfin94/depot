793aa1ea9d10edf65c2d17909097281ec173ef5c
Sherfin S <sherfin94@gmail.com>
2016-08-09 17:58:50 +0530

    Add cart to sidebar
    This commit adds moves the user's shopping cart to the side bar.
    It automatically updates when each item is added.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,6 +11,7 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    @sum = @cart.line_items.to_a.sum {|item| item.total_price}
   end
 
   # GET /carts/new
@@ -55,9 +56,10 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Cart was successfully emptied' }
       format.json { head :no_content }
     end
   end
