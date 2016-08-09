49307fe1c7351672929bdbae03a62e2030e63a5a
Sherfin S <sherfin94@gmail.com>
2016-08-09 11:55:02 +0530

    Permit only product_id parameter for line_items
    This commit ensures that only the product_id paramter is permitted for fetching
    line items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,15 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+
+    if not current_item.nil?
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+      current_item.quantity = 1
+    end
+    current_item
+  end
 end
