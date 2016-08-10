c05b9d3a12c433a3d3ecca2294b19296abbe72d0
Sherfin S <sherfin94@gmail.com>
2016-08-10 13:59:13 +0530

    Make the Cart ajax based

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -34,6 +34,7 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url, notice: 'Item Added' }
+        format.js {render layout: false}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
